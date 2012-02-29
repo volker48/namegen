9ebb72eab3e3a1ab12073736d94cabf7a8a36b56
Marcus McCurdy <marcus.mccurdy@gmail.com>
2012-02-29 15:13:16 -0500

    Modified namegen to use bigrams

--- namegen.py
+++ namegen.py
@@ -24,6 +24,9 @@ class Namegen(object):
     def to_ordinal(self, c):
         return 0 if c == ' ' else ord(c.lower()) - 96
 
+    def bi_to_ordinal(self, bi):
+        return 27 * self.to_ordinal(bi[0]) + self.to_ordinal(bi[1])
+
     def from_ordinal(self, i):
         return ' ' if i == 0 else chr(i + 96)
 
@@ -32,21 +35,21 @@ class Namegen(object):
             return self.create_prob(file)
 
     def create_prob(self, file):
-        prob = numpy.zeros((27,27), dtype=numpy.int16)
+        prob = numpy.zeros((729, 27), dtype=numpy.int16)
         for line in file:
             line = line.rstrip()
             if not line.isalpha():
                 continue
-            for i in xrange(len(line)):
-                prev = self.to_ordinal(line[i - 1]) if i > 0 else 0
+            #two in the front one in the back
+            line = '  ' + line + ' '
+            for i in xrange(2, len(line)):
+                prev =self.bi_to_ordinal(line[i - 2:i])
                 cur = self.to_ordinal(line[i])
                 prob[prev, cur] += 1
-                if i == len(line) - 1:
-                    prob[cur, 0] += 1
-        return prob, numpy.sum(prob, axis=0)
+        return prob, numpy.sum(prob, axis=1)
 
     def pick_char(self, previous):
-        ordinal = self.to_ordinal(previous)
+        ordinal = self.bi_to_ordinal(previous)
         total = self.sums[ordinal]
         if not total:
             return ' '
@@ -59,14 +62,11 @@ class Namegen(object):
 
 
     def generate(self):
-        name = ''
-        current = ' '
-        current = self.pick_char(current)
-        name += current
-        while current != ' ':
-            current = self.pick_char(current)
-            name += current
-        return name.capitalize()
+        name = '  '
+        name += self.pick_char(name[-2:])
+        while name[-1] != ' ':
+            name += self.pick_char(name[-2:])
+        return name.strip().capitalize()
 
 
 
